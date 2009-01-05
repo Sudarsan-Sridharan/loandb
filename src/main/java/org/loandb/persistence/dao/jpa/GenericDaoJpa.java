f13eefab567a3a249d68d79ba642dfbc2848c9e4
arulazi.d <arulazi.d@33703f68-daaa-11dd-918e-25ffe7a15323>
2009-01-05 02:59:49 +0000

    Adding initial import of the loandb persistence layer with a simple CrudTest.

---
+++ src/main/java/org/loandb/persistence/dao/jpa/GenericDaoJpa.java
@@ -0,0 +1,56 @@
+package org.loandb.persistence.dao.jpa;
+
+import org.loandb.persistence.dao.GenericDao;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * LoanDB project (http://code.google.com/p/loandb/)
+ *
+ * @author <a href="mailto:aruld@acm.org">Arulazi Dhesiaseelan</a>
+ * @since Jan 4, 2009
+ */
+public abstract class GenericDaoJpa<T, PK extends Serializable> implements GenericDao<T, PK> {
+
+  @PersistenceContext
+  protected EntityManager entityManager;
+
+  private Class<T> persistentClass;
+
+  public GenericDaoJpa(Class<T> persistentClass) {
+    this.persistentClass = persistentClass;
+  }
+
+  public List<T> getAll() {
+    return this.entityManager.createQuery(
+      "select entity from " + this.persistentClass.getName() + " entity")
+      .getResultList();
+  }
+
+  public T get(PK id) {
+    T entity = this.entityManager.find(this.persistentClass, id);
+    return entity;
+  }
+
+  public boolean exists(PK id) {
+    T entity = this.entityManager.find(this.persistentClass, id);
+    return entity != null;
+  }
+
+  public T save(T entity) {
+    this.entityManager.persist(entity);
+    return entity;
+  }
+
+  public T update(T entity) {
+    return this.entityManager.merge(entity);
+  }
+
+
+  public void remove(PK id) {
+    this.entityManager.remove(this.get(id));
+  }
+}
---
+++ src/test/java/org/loandb/persistence/AbstractTest.java
@@ -0,0 +1,47 @@
+package org.loandb.persistence;
+
+import org.junit.runner.RunWith;
+import org.loandb.persistence.service.ApplicationService;
+import org.loandb.persistence.service.CreditService;
+import org.loandb.persistence.service.DecisionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.transaction.TransactionConfiguration;
+import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.PersistenceContext;
+import javax.persistence.PersistenceUnit;
+
+/**
+ * LoanDB project (http://code.google.com/p/loandb/)
+ *
+ * @author <a href="mailto:aruld@acm.org">Arulazi Dhesiaseelan</a>
+ * @since Jan 4, 2009
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@Transactional
+@TransactionConfiguration(transactionManager = "transactionManager")
+@TestExecutionListeners({TransactionalTestExecutionListener.class})
+@ContextConfiguration(locations = {"classpath:/spring/spring-config.xml"})
+public abstract class AbstractTest extends AbstractTransactionalJUnit4SpringContextTests {
+  @PersistenceContext
+  protected EntityManager entityManager;
+
+  @PersistenceUnit(name = "loandb")
+  protected EntityManagerFactory entityManagerFactory;
+
+  @Autowired
+  protected ApplicationService applicationService;
+
+  @Autowired
+  protected CreditService creditService;
+
+  @Autowired
+  protected DecisionService decisionService;
+}
